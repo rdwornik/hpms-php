ae4149ceff027410f64561ad6026c1e9467f278c
robertdwornik <robert.dwornik@outlook.com>
2020-02-12 15:26:18 +0100

    wygląda ok2

--- hpmsPHP/LogPlugin.php
+++ hpmsPHP/LogPlugin.php
@@ -1,22 +1,10 @@
 <?php
 // include "models/Log.class.php";
-include "models/Log2.class.php";
+include "models/Log.class.php";
 
-// include "DBconnection.class.php";
-
-// $dbcon = new DBconnection();
-// $db = $dbcon->getdb();
-// $log = new Log($db);
-// $res = $log->save();
 echo "log2";
-$log2 = new Log2();
-$log2->isStrange('roveer');
+$log = new Log();
 $res = $log2->save();
-//print_r($res);
-// if (!$res)
-// {
-//     echo "Can't save all logs to a database";
-//     die();
-// }
+
 
 echo $config['notice'];
--- hpmsPHP/LogPluginSilent.php
+++ hpmsPHP/LogPluginSilent.php
@@ -1,15 +1,15 @@
 <?php
-include "models/Log.class.php";
-include "DBconnection.class.php";
+// include "models/Log.class.php";
+// include "DBconnection.class.php";
 
-$dbcon = new DBconnection();
-$db = $dbcon->getdb();
-$log = new Log($db);
-$res = $log->save();
+// $dbcon = new DBconnection();
+// $db = $dbcon->getdb();
+// $log = new Log($db);
+// $res = $log->save();
 
 
-if (!$res)
-{
-    echo "Can't save all logs to a database";
-    die();
-}
+// if (!$res)
+// {
+//     echo "Can't save all logs to a database";
+//     die();
+// }
