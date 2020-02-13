2ea1d787b41ff4099b2d455cc47ca95a1b44f39b
robertdwornik <robert.dwornik@outlook.com>
2020-02-13 17:52:07 +0100

    working post by using curl

--- hpmsPHP/models/Log.class.php
+++ hpmsPHP/models/Log.class.php
@@ -60,40 +60,60 @@ class Log
             }
             $tmp = array_merge($tmp,array($key => $value));
         }
-        return $tmp;
+        return array("data" => $tmp);
     }
 
     public function post()
     {
-
-        //Transform our POST array into a URL-encoded query string.
-        $postStr = http_build_query($this->getLog());
-        $this->setContentLength($postStr);
+        // print_r($this->getLog());
+        // //Transform our POST array into a URL-encoded query string.
+        // $postStr = http_build_query($this->getLog());
+        // $this->setContentLength($postStr);
         
-        $options = array(
-            'http' =>
-                array(
-                    'method'  => 'POST', //We are using the POST HTTP method.
-                    'header'  =>  array_map(function ($h, $v) {return "$h: $v";}, array_keys($this->headers), $this->headers),
-                    'content' => $postStr //Our URL-encoded query string.
-                    )
-        );
-        //Pass our $options array into stream_context_create.
-        //This will return a stream context resource.
-        $streamContext  = stream_context_create($options);
+        // $options = array(
+        //     'http' =>
+        //         array(
+        //             'method'  => 'POST', //We are using the POST HTTP method.
+        //             'header'  =>  array_map(function ($h, $v) {return "$h: $v";}, array_keys($this->headers), $this->headers),
+        //             'content' => $postStr //Our URL-encoded query string.
+        //             )
+        // );
+        // //Pass our $options array into stream_context_create.
+        // //This will return a stream context resource.
+        // $streamContext  = stream_context_create($options);
 
-        //Use PHP's file_get_contents function to carry out the request.
-        //We pass the $streamContext variable in as a third parameter.
-        $result = file_get_contents( $this->url, false, $streamContext);
-        //If $result is FALSE, then the request has failed.
-        if($result === false)
-        {
-            //If the request failed, throw an Exception containing
-            //the error.
-            $error = error_get_last();
-            throw new Exception('POST request failed: ' . $error['message']);
-        }
+        // //Use PHP's file_get_contents function to carry out the request.
+        // //We pass the $streamContext variable in as a third parameter.
+        // $result = file_get_contents( $this->url, false, $streamContext);
+        // //If $result is FALSE, then the request has failed.
+        // if($result === false)
+        // {
+        //     //If the request failed, throw an Exception containing
+        //     //the error.
+        //     $error = error_get_last();
+        //     throw new Exception('POST request failed: ' . $error['message']);
+        // }
         //If everything went OK, return the response.
+        $h =  array_map(function ($h, $v) {return "$h: $v";}, array_keys($this->headers), $this->headers);                                      
+        $t = json_encode($this->getLog());
+        print_r($t);        
+        echo "\n";
+        $body = '{
+            "kind": "blogger#post",
+            "blog": {
+              "id": "8070105920543249955"
+            },
+            "title": "A new post",
+            "data": "With <b>exciting</b> content..."
+          }';
+          $ch = curl_init();
+          curl_setopt($ch, CURLOPT_USERPWD, "user2:user2user2user2");  
+          curl_setopt($ch, CURLOPT_URL, $this->url);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($ch, CURLOPT_HTTPHEADER,$h);
+          curl_setopt($ch, CURLOPT_POST, 1);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $t);
+          $result = curl_exec($ch);
         return $result;
     }
 
