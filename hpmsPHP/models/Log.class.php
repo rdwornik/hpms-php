7c3cbf2c6e34582393848a3c467c101817723be6
robertdwornik <robert.dwornik@outlook.com>
2020-02-14 10:11:46 +0100

    curl version

--- hpmsPHP/models/Log.class.php
+++ hpmsPHP/models/Log.class.php
@@ -13,6 +13,7 @@ class Log
     private $strangeStr; //strange info string
     private $url;
     private $headers;
+    private $auth;
 
     public function __construct()
     {
@@ -27,6 +28,7 @@ class Log
         $this->strangeStr = $config['strangeStr'];
         $this->url = $config['url'];
         $this->headers = (array)$config['headers'];
+        $this->auth = $config['user'].":".$config['password'];
     }
     private function getLog()
     {
@@ -65,39 +67,12 @@ class Log
 
     public function post()
     {
-        // print_r($this->getLog());
-        // //Transform our POST array into a URL-encoded query string.
-        // $postStr = http_build_query($this->getLog());
-        // $this->setContentLength($postStr);
-        
-        // $options = array(
-        //     'http' =>
-        //         array(
-        //             'method'  => 'POST', //We are using the POST HTTP method.
-        //             'header'  =>  array_map(function ($h, $v) {return "$h: $v";}, array_keys($this->headers), $this->headers),
-        //             'content' => $postStr //Our URL-encoded query string.
-        //             )
-        // );
-        // //Pass our $options array into stream_context_create.
-        // //This will return a stream context resource.
-        // $streamContext  = stream_context_create($options);
-
-        // //Use PHP's file_get_contents function to carry out the request.
-        // //We pass the $streamContext variable in as a third parameter.
-        // $result = file_get_contents( $this->url, false, $streamContext);
-        // //If $result is FALSE, then the request has failed.
-        // if($result === false)
-        // {
-        //     //If the request failed, throw an Exception containing
-        //     //the error.
-        //     $error = error_get_last();
-        //     throw new Exception('POST request failed: ' . $error['message']);
-        // }
         //If everything went OK, return the response.
         $header =  array_map(function ($h, $v) {return "$h: $v";}, array_keys($this->headers), $this->headers);
         $body = json_encode($this->getLog());
+        $this->setContentLength($body);
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_USERPWD, "user2:user2user2user2");  
+        curl_setopt($ch, CURLOPT_USERPWD, $this->auth);  
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER,$header);
