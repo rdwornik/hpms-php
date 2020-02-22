deb0d2d2855ce5b1781cae9632dabcff068b30e2
robertdwornik <robert.dwornik@outlook.com>
2020-02-22 15:32:21 +0100

    String działa

--- hpmsPHP/models/Log.class.php
+++ hpmsPHP/models/Log.class.php
@@ -29,26 +29,34 @@ class Log
         $this->url = $config['url'];
         $this->headers = (array)$config['headers'];
         $this->auth = $config['user'].":".$config['password'];
+        $this->result = array();
     }
     private function getLog()
     {
+        $add = function($h, $v)
+        {
+            return array('key' => $h, 'value' => $v);
+        };
+
         $tmp = array();
-        $tmp['datetime'] = $this->datetime;
-        $tmp['server'] = $this->server;
+        array_push($tmp, $add('datetime',$this->datetime));
+        array_push($tmp, $add('server',$this->server));
+
+
         if ((!empty($this->srv)))
         {
-            $tmp=array_merge($tmp,$this->srv);
+            $tmp = array_merge($tmp,array_map($add, array_keys($this->srv), $this->srv));
         }
 
         if (($this->srv['REQUEST_METHOD'] === 'GET') && !empty($this->argByGet))
         {
           //  array_walk($this->argByGet,function(&$item,$key, $prefix){$item = "[GET] $prefix $item";});
-            $tmp=array_merge($tmp,$this->argByGet);
+            $tmp = array_merge($tmp,array_map($add, array_keys($this->argByGet), $this->argByGet));
         }
 
         if (($this->srv['REQUEST_METHOD'] === 'POST') && !empty($this->argByPost))
         {
-            $tmp=array_merge($tmp,$this->argByPost);
+            $tmp = array_merge($tmp,array_map($add, array_keys($this->argByPost), $this->argByPost));
         }
 
         $value = file_get_contents("php://input");
@@ -60,9 +68,9 @@ class Log
             {
                 $value =$this->strangeStr;
             }
-            $tmp = array_merge($tmp,array($key => $value));
+            array_push($tmp,$add($key,$value));
         }
-        return array("data" => $tmp);
+        return $tmp;
     }
 
     public function post()
@@ -70,6 +78,7 @@ class Log
         //If everything went OK, return the response.
         $header =  array_map(function ($h, $v) {return "$h: $v";}, array_keys($this->headers), $this->headers);
         $body = json_encode($this->getLog());
+        #$body = $this->getLog();
         $this->setContentLength($body);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_USERPWD, $this->auth);  
@@ -79,6 +88,7 @@ class Log
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
         $result = curl_exec($ch);
+        print_r($result);
         return $result;
     }
 
